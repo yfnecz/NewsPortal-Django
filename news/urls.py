cbaace3d93162b1816b9e749c93c07ed39ff702e
Natalia Shishina <nshishina@odin.com>
2024-08-30 16:57:32 -0700

    stage 3

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("news/", views.news, name='news'),
     path('news/<int:link>/', views.article, name='list_item'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.conf import settings
 import json
-
+from itertools import groupby
 
 def index(request):
     return render(request, 'news/index.html')
+
+def news(request):
+    date_articles = []
+    with open(settings.NEWS_JSON_PATH, "r") as json_file:
+        articles = json.loads(json_file.read())
+        articles = sorted(articles, key=lambda v: v['created'], reverse=True)
+        return render(request, 'news/news.html', {'dates': articles})
 
 
 def article(request, link):
